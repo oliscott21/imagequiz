4afc92192ce1dc729adbbff2a3e5df5e2efb415d
oliscott21 <oliscott21@gmail.com>
2022-05-04 22:29:05 -0700

    v4.2 - fixes

--- imagequiz/src/configurations.js
+++ imagequiz/src/configurations.js
@@ -1,5 +1,5 @@
 let congifuration = {
-    backendAddress: "https://oliscott21-imagequiz-backend.herokuapp.com"
+    backendAddress: "https://oliscott21-imagequiz-api.herokuapp.com"
 };
 
 export default congifuration;
